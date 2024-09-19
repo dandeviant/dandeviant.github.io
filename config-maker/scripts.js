8c65b95c6a43bc8d407c0116b65306f9abb8ed3a
Muhammad Danial <muhammaddanial63@gmail.com>
2024-09-19 17:13:55 +0800

    edit index

--- config-maker/scripts.js
+++ config-maker/scripts.js
@@ -9,7 +9,40 @@ function generateScript() {
     vlanQUA = document.getElementById('vlanQUAInput').value;
 
     error = "";
+    document.getElementById('scriptGenerated').value = "";
+    document.getElementById('scriptGenerated').placeholder = "";
 
+    scriptresult = "";
+
+    scriptresult = vlanInput(scriptresult);
+
+    scriptresult += "\n\n";
+
+    scriptresult = selectSwitchModel(scriptresult);
+
+    document.getElementById("scriptGenerated").value = scriptresult;
+
+}   
+
+
+function selectSwitchModel (scriptresult) {
+    selectModel = document.getElementById('selectSwitchModel').value;
+    if (selectModel == "")  {
+        console.log("NO SWITCH SELECTED");
+        scriptresult += "===================== ERROR =====================\n";
+        scriptresult += "NO SWITCH SELECTED" + "\n";
+        scriptresult += "===================== ERROR =====================\n";
+    }
+    else {
+        console.log("Switch Selected = " + selectModel);
+        scriptresult += "Switch Selected = " + selectModel + "\n";
+    }
+
+    return scriptresult;
+}
+
+
+function vlanInput (scriptresult) {
     if (vlanCCTV == "" || vlanISC == "" || vlanQUA == "") {
         if (vlanCCTV == ""){
             if (error != ""){
@@ -30,18 +63,19 @@ function generateScript() {
             error += "QUA VLAN"
         }
         console.log(error + " is missing");
+        scriptresult += "===================== ERROR =====================\n";
+        scriptresult += error + " is missing \n";
+        scriptresult += "===================== ERROR =====================\n";
     }
     else {
         console.log("VLAN CCTV = " + vlanCCTV);
         console.log("VLAN ISC  = " + vlanISC);
         console.log("VLAN QUA  = " + vlanQUA);
+
+        scriptresult += "VLAN CCTV = " + vlanCCTV + "\n";
+        scriptresult += "VLAN ISC  = " + vlanISC  + "\n";
+        scriptresult += "VLAN QUA  = " + vlanQUA  + "\n";
     }
 
-    selectModel = document.getElementById('selectSwitchModel').value;
-    if (selectModel == "")  {
-        console.log("NO SWITCH SELECTED");
-    }
-    else {
-        console.log("Value = " + selectModel);
-    }
-}   
+    return scriptresult;
+}
