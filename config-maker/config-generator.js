5ecf54dad4a40862e5612f7e510669e25fd43ed3
dandeviant <muhammaddanial63@gmail.com>
2024-09-23 18:46:30 +0800

    phase 1 complete

--- config-maker/config-generator.js
+++ config-maker/config-generator.js
@@ -6,8 +6,10 @@ function vlanSplit() {
     if (vlanInputText == "") {
         $('#modalCopyAlert').modal('show'); //display modal alert
         document.getElementById('modalAlert').innerHTML = '<div class="alert alert-danger" role="alert">No VLAN detected</div>';
+        resetScript();
         return;
     }
+    
     const vlanArrayText = vlanInputText.split(/\s+/);
     const vlanArrayNum = new Array();
     console.log(vlanArrayText[0]);
@@ -15,26 +17,33 @@ function vlanSplit() {
 
     for (x = 0; x < totalVlan; x++){
         if (vlanArrayText[x] != ""){ 
-            vlanArrayNum[x] = Number(vlanArrayText[x]);
+            if (vlanArrayText[x] == "trunk") {
+                continue;
+            }
+            else {
+                // vlanArrayNum = Number(vlanArrayText[x]);
+                if (Number.isNaN(vlanArrayNum) == true) {
+                    $('#modalCopyAlert').modal('hide'); //hide modal alert
+                    document.getElementById('modalAlert').innerHTML = '<div class="alert alert-danger" role="alert">String detected in VLAN list. Generation aborted</div>';
+                    $('#modalCopyAlert').modal('show'); //display modal alert
+                    // document.getElementById('modalAlert').innerHTML = '<div class="alert alert-success" role="alert">Copied to clipboard</div>';
+                    // inputAlert();
+                    return;
+                }
+            }
+            
         }
-        if (Number.isNaN(vlanArrayNum[x]) == true) {
-            $('#modalCopyAlert').modal('hide'); //hide modal alert
-            document.getElementById('modalAlert').innerHTML = '<div class="alert alert-danger" role="alert">String detected in VLAN list. Generation aborted</div>';
-            $('#modalCopyAlert').modal('show'); //display modal alert
-            // document.getElementById('modalAlert').innerHTML = '<div class="alert alert-success" role="alert">Copied to clipboard</div>';
-            // inputAlert();
-            return;
-        }
+        
     }
     
     $('#modalCopyAlert').modal('hide');
     copyText = "";
-    console.log("vlanArrayNum = " + vlanArrayNum);
+    console.log("vlanArrayText = " + vlanArrayText);
     for (x = 0; x < totalVlan; x++){
-        console.log("Vlan " + (x+1) + ": " + vlanArrayNum[x]);
-        copyText += "Vlan " + (x+1) + ": " + vlanArrayNum[x] + "\n";
+        console.log("Vlan " + (x+1) + ": " + vlanArrayText[x]);
+        copyText += "Vlan " + (x+1) + ": " + vlanArrayText[x] + "\n";
     }
-    copyText = intCommand(copyText, vlanArrayNum, totalVlan);
+    copyText = generateScript(copyText, vlanArrayText, totalVlan);
     copyText += '!############### END OF SCRIPT ###############';
     document.getElementById('scriptGenerated').value = copyText;
 }
@@ -47,19 +56,50 @@ function inputAlert() {
 }
 
 
-function intCommand(copyText, vlanArrayNum, totalVlan) {
+function generateScript(copyText, vlanArrayText, totalVlan) {
+    vlanCCTV = document.getElementById('vlanCCTVInput').value;
+    vlanISC = document.getElementById('vlanISCInput').value;
+    vlanQUA = document.getElementById('vlanQUAInput').value;
+    vlanNative = document.getElementById('vlanNativeInput').value;
+
+    error = "";
+    document.getElementById('scriptGenerated').value = "";
+    document.getElementById('scriptGenerated').placeholder = "";
+
     copyText = "";
-    // for (x = 0; x < totalVlan; x++){
-    //     console.log("Vlan intCommand " + (x+1) + ": " + vlanArrayNum[x]);
-    //     copyText += "Vlan intCommand " + (x+1) + ": " + vlanArrayNum[x] + "\n";
-    // }
+ 
     for (x = 0; x < totalVlan; x++){
-        if (vlanArrayNum[x] != undefined){
-            copyText += 'interface GigabitEthernet0/' + (x+1);
-            copyText += '\n  switchport access vlan ' + vlanArrayNum[x] + '\n';
+        if (vlanArrayText[x] != undefined || vlanArrayText[x] == ""){
+            
+            if (vlanArrayText[x] != "trunk"){
+                copyText += 'interface GigabitEthernet0/' + (x+1);
+            }
+
+            if (vlanArrayText[x] == vlanCCTV){
+                copyText += '\ndescription #CCTV';
+            }
+            else if (vlanArrayText[x] == vlanISC){
+                copyText += '\ndescription #ISC';
+            }
+            else if (vlanArrayText[x] == vlanQUA){
+                copyText += '\n  switchport access vlan ' + vlanNative + '\n';
+                copyText += '!\n';
+                continue;
+            }
+            else if (vlanArrayText[x] == "trunk"){
+                copyText += 'exit\n';
+                copyText += 'default interface GigabitEthernet0/' + (x+1) + '\n';
+                copyText += 'interface GigabitEthernet0/' + (x+1);
+                copyText += '\n  switchport mode trunk\n';
+                copyText += '  switchport trunk native vlan ' + vlanNative + '\n';
+                copyText += '!\n';
+                continue;
+            }
+            copyText += '\n  switchport access vlan ' + vlanArrayText[x] + '\n';
             copyText += '!\n';
-        }   
+        }
+        
         
     }
     return copyText;
-}
+}  
--- config-maker/index.js
+++ config-maker/index.js
@@ -1,44 +1,20 @@
 function resetScript() { 
     document.getElementById('scriptGenerated').value = '';
     document.getElementById('scriptAlert').innerHTML = "";
+    document.getElementById('scriptGenerated').placeholder = "Generated Script Here";
 }
 
 function switchModel() {selectModel = document.getElementById('selectSwitchModel').value;}
-
-function generateScript() {
-    vlanCCTV = document.getElementById('vlanCCTVInput').value;
-    vlanISC = document.getElementById('vlanISCInput').value;
-    vlanQUA = document.getElementById('vlanQUAInput').value;
-
-    error = "";
-    document.getElementById('scriptGenerated').value = "";
-    document.getElementById('scriptGenerated').placeholder = "";
-
-
-
-    // scriptresult = "";
-    // scriptresult = vlanInput(scriptresult);
-    // scriptresult += "\n\n";
-    // scriptresult = vlanDefault(scriptresult);
-    // scriptresult += "\n\n";
-    // scriptresult = selectSwitchModel(scriptresult);
-
-    // document.getElementById("scriptGenerated").value = scriptresult;
-
-}   
+ 
 
 function vlanInput (scriptresult) {
     vlanInputText = document.getElementById('vlanInput').value;
 
     if (vlanInputText == "") {
-        // scriptresult += "===================== ERROR =====================\n";
-        // scriptresult += "NO VLAN INPUT FOUND" + "\n";
-        // scriptresult += "===================== ERROR =====================\n";
         inputAlert();
     }
     else {
         scriptresult += vlanInputText;
-        // scriptresult += "\n\n";
     }
 
     return scriptresult;
@@ -48,13 +24,9 @@ function selectSwitchModel (scriptresult) {
     selectModel = document.getElementById('selectSwitchModel').value;
     if (selectModel == "")  {
         console.log("NO SWITCH SELECTED");
-        // scriptresult += "===================== ERROR =====================\n";
-        // scriptresult += "NO SWITCH SELECTED" + "\n";
-        // scriptresult += "===================== ERROR =====================\n";
     }
     else {
         console.log("Switch Selected = " + selectModel);
-        // scriptresult += "Switch Selected = " + selectModel + "\n";
     }
 
     return scriptresult;
@@ -82,18 +54,13 @@ function vlanDefault (scriptresult) {
             error += "QUA VLAN"
         }
         console.log(error + " is missing");
-        // scriptresult += "===================== ERROR =====================\n";
-        // scriptresult += error + " is missing \n";
-        // scriptresult += "===================== ERROR =====================\n";
+
     }
     else {
         console.log("VLAN CCTV = " + vlanCCTV);
         console.log("VLAN ISC  = " + vlanISC);
         console.log("VLAN QUA  = " + vlanQUA);
 
-        // scriptresult += "VLAN CCTV = " + vlanCCTV + "\n";
-        // scriptresult += "VLAN ISC  = " + vlanISC  + "\n";
-        // scriptresult += "VLAN QUA  = " + vlanQUA  + "\n";
     }
 
     return scriptresult;
@@ -118,47 +85,9 @@ function scriptAlert() {
     }
     else {
         document.getElementById('modalAlert').innerHTML = '<div class="alert alert-success" role="alert">Copied to clipboard</div>';
-        // document.getElementById('modalAlert').innerHTML = "Copied to Clipboard";
-        // document.getElementById('scriptAlert').innerHTML = '\
-        // <div class="row">\
-        //     <div class="col">\
-        //         <div class="alert alert-success" role="alert">\
-        //             Script Copied\
-        //         </div>\
-        //     </div>\
-        // </div>'
     }    
 }
 
 function clearAlert() {
     document.getElementById('scriptAlert').innerHTML = "";
 }
-
-
-// function vlanSplit() {
-//     vlanInputText = document.getElementById('vlanInput').value;
-//     const vlanArrayText = vlanInputText.split(" ");
-//     const vlanArrayNum = new Array();
-//     console.log(vlanArrayText[0]);
-
-//     totalVlan = vlanArrayText.length;
-
-//     for (x = 0; x < totalVlan; x++){
-//         vlanArrayNum[x] = Number(vlanArrayText[x]);
-//         if (Number.isNaN(vlanArrayNum[x]) == false) {
-//             console.log("String Found. Aborted");
-//             document.getElementById('modalAlert').innerHTML = '<div class="alert alert-success" role="alert">Copied to clipboard</div>';
-//             inputAlert();
-//             return;
-//         }
-//     }
-
-//     for (x = 0; x < totalVlan; x++){
-//         console.log("Vlan " + (x+1) + ": " + vlanArrayNum[x]);
-//     }
-// }
-
-// function inputAlert() {
-//     document.getElementById('modalAlert').innerHTML = '<div class="alert alert-danger" role="alert">String detected in sdasdasd list. Generation aborted</div>';
-//     // document.getElementById('modalAlert').innerHTML = '<div class="alert alert-success" role="alert">Copied to clipboard </div>';
-// }
