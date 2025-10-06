e661b2e10f967b53278d08f2cc9db16a5ace0754
dandeviant <muhammaddanial63@gmail.com>
2025-10-06 15:37:21 +0800

    update

---
+++ _next/static/chunks/95324894dab6cabb.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/6984fb5cfcd20ac3.js",
+  "static/chunks/turbopack-001f0d3e05286596.js"
+])
---
+++ _next/static/chunks/f4ed1642510840ef.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/6984fb5cfcd20ac3.js",
+  "static/chunks/turbopack-dc07cfb498b9f6f3.js"
+])
